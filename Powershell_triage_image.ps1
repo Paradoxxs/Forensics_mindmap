aaf26ebc934a6b6f2d8c2ebfa61155ed41d0eb9b
Paradoxxs <Aston.dk@gmail.com>
2022-04-13 12:56:46 +0200

    Add PS for creating an quick triage image

--- Powershell_triage_image.ps1
+++ Powershell_triage_image.ps1
@@ -1,33 +1,31 @@
-﻿
+﻿$staging = "c:\staging\"
+New-Item -Path $staging -ItemType Directory
+$destination = "c:\triage_image.zip"
+
 $paths = Get-ChildItem -Path $env:SYSTEMROOT\System32\winevt\Logs\
-$paths = Get-ChildItem -Path $env:SYSTEMROOT\System32\winevt\config\
-$paths = Get-item -Path $env:SYSTEMROOT\AppCompat\Programs\Amcache.hve  
-$paths = Get-item -Path $env:SYSTEMROOT\System32\SRUDB.dat
-$paths = Get-ChildItem -Path $env:userprofile\ -include ntuser.dat*
-$paths = Get-ChildItem -Path $env:userprofile\AppData\Local\Microsoft\Windows -include userclass.dat*
-$paths = Get-ChildItem -Path $env:userprofile\AppData\Local\Microsoft\Windows\Explorer -include thumbcache*
-$paths = Get-ChildItem -Path $env:userprofile\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadline\ConsoleHost_history.txt
-$paths = Get-ChildItem -Path $env:userprofile\AppData\Roaming\Mozilla\Firefox\Profiles\
-$paths = Get-ChildItem -Path $env:userprofile\AppData\Roaming\Microsoft\Windows\Recent\
-$paths = Get-ChildItem -Path $env:userprofile\AppData\Roaming\Microsoft\Office\Recent\  
-$paths = Get-ChildItem -Path $env:userprofile\AppData\Local\Microsoft\Windows\WebCache\
-$paths = Get-ChildItem -Path $env:userprofile\AppData\Local\Microsoft\Windows\Explorer\
-$paths = Get-ChildItem -Path $env:userprofile\AppData\Local\ConnectedDevicesPlatform\
-$paths = Get-ChildItem -Path $env:userprofile\AppData\Local\Google\Chrome\User Data\Default\History\
-$paths = Get-ChildItem -Path $env:userprofile\AppData\Local\Microsoft\Edge\User Data\Default\History\
-$paths = Get-ChildItem -Path $env:userprofile\AppData\Roaming\Microsoft\Windows\Recent\AutomaticDestinations
-$paths = Get-ChildItem -Path $env:userprofile\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt
-$paths = Get-item -Path $env:userprofile\AppData\Local\ConnectedDevices  Platform\ActivitiesCache.db  
-$paths = Get-ChildItem -Path $env:userprofile\AppData\Local\Microsoft\Windows\ -include UsrClass.dat*
-$paths = Get-ChildItem -Path $env:programdata\Microsoft\Windows\Start Menu\Programs\Startup\
-$paths = Get-item -Path $env:programdata\microsoft\search\data\application\windows\windows.edb
+$paths += Get-ChildItem -Path $env:SYSTEMROOT\System32\config\
+$paths += Get-item -Path $env:SYSTEMROOT\AppCompat\Programs\Amcache.hve  
+$paths += Get-item -Path $env:SYSTEMROOT\System32\sru\SRUDB.dat
+$paths += Get-ChildItem -Path $env:userprofile\ -include ntuser.dat*
+$paths += Get-ChildItem -Path $env:userprofile\AppData\Local\Microsoft\Windows -include userclass.dat*
+$paths += Get-ChildItem -Path $env:userprofile\AppData\Local\Microsoft\Windows\Explorer -include thumbcache*
+$paths += Get-ChildItem -Path $env:userprofile\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadline\ConsoleHost_history.txt
+$paths += Get-ChildItem -Path $env:userprofile\AppData\Roaming\Microsoft\Windows\Recent\
+$paths += Get-ChildItem -Path $env:userprofile\AppData\Roaming\Microsoft\Office\Recent\  
+$paths += Get-ChildItem -Path $env:userprofile\AppData\Local\Microsoft\Windows\WebCache\
+$paths += Get-ChildItem -Path $env:userprofile\AppData\Local\Microsoft\Windows\Explorer\
+$paths += Get-ChildItem -Path $env:userprofile\AppData\Local\ConnectedDevicesPlatform\
+$paths += Get-ChildItem -Path $env:userprofile\AppData\Roaming\Microsoft\Windows\Recent\AutomaticDestinations
+$paths += Get-ChildItem -Path $env:userprofile\Appdata\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt
+$paths += Get-ChildItem -Path $env:userprofile\AppData\Local\Microsoft\Windows\ -include UsrClass.dat*
 
-ForEach ($Path in $paths) 
+
+ForEach ($Path in $paths ) 
 {
-
-Get-ChildItem $path.fullname -Recurse |foreach{
-
-Compress-Archive -Path $_.FullName -DestinationPath "C:\triage_iamge.zip" -update -CompressionLevel Optimal
-
- }
+    if (Test-Path $Path.FullName) {
+        robocopy $Path.DirectoryName $staging $Path.Name
+    }
 }
+
+Get-ChildItem -Path $staging -Recurse | Compress-Archive -DestinationPath $destination -Force -ErrorAction Continue
+Remove-Item -Path $staging -Recurse -Force
